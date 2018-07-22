fd25c0dff1025ca4f8f1366eda133a902a929582
WMXPY <wm@mengw.io>
2018-07-21 19:18:27 -0500

    done

--- src/example.ts
+++ src/example.ts
@@ -6,9 +6,13 @@
 import Markus from "./index";
 
 const m = new Markus('http://localhost:8080');
-
 document.getElementById('btn').addEventListener('click', () => {
-    const part: HTMLInputElement = document.getElementById('a') as HTMLInputElement;
-    const file = part.files[0];
-    m.uploadSingleImageByFile(file, 'test', ['sdk-b']);
+    const element: HTMLInputElement = document.getElementById('a') as HTMLInputElement;
+    const file: FileList = element.files;
+    console.log(file);
+    m.uploadMultipleImageByFileList(file, 'test', ['sdk-b']).then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    })
 });
--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ export interface IMarkusResponse {
 
 export default class Markus {
     private domain: string;
-    constructor(domain: string) {
+    public constructor(domain: string) {
         this.domain = domain;
     }
 
-    uploadSingleImageByFile(file: File, key: string, tags: string[]): Promise<IMarkusResponse> {
+    public uploadSingleImageByFile(file: File, key: string, tags: string[]): Promise<IMarkusResponse> {
         return new Promise<IMarkusResponse>((resolve, reject) => {
             let data = new FormData();
 
@@ -33,16 +33,60 @@ export default class Markus {
                 method: 'POST',
                 mode: 'cors',
                 body: data,
-            })
-                .then(function (response: Response) {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        console.log(response);
-                    }
-                }).then(function (data: IMarkusResponse) {
+            }).then(function (response: Response) {
+                return response.json();
+            }).then(function (data: IMarkusResponse) {
+                if (data.status === MARKUS_RESPONSE.SUCCEED) {
                     resolve(data);
+                } else {
+                    reject(data);
+                }
+            });
+        });
+    }
+
+    public uploadMultipleImageByFiles(files: File[], key: string, tags: string[]): Promise<IMarkusResponse[]> {
+        const resultList: IMarkusResponse[] = [];
+        const filesLength = files.length;
+        const Upload = (callback: () => any, whenErr: (err: Error) => any) => {
+            const file = files.shift();
+            if (file) {
+                this.uploadSingleImageByFile(file, key, tags).then((result: any) => {
+                    resultList.push(result);
+                    Upload(callback, whenErr);
+                }).catch((err: Error) => {
+                    whenErr(err);
                 });
-        })
+            } else {
+                callback();
+            }
+        };
+
+        return new Promise<IMarkusResponse[]>((resolve, reject) => {
+            const loop = files.length <= 5 ? files.length : 5;
+            for (let i = 0; i < loop; i++) {
+                Upload(() => {
+                    if (resultList.length === filesLength) {
+                        resolve(resultList);
+                    }
+                }, (err: Error) => {
+                    reject(err);
+                });
+            }
+        });
+    }
+
+    public uploadMultipleImageByFileList(fileList: FileList, key: string, tags: string[]): Promise<IMarkusResponse[]> {
+        const files: File[] = []
+        for (let i = 0; i < fileList.length; i++) {
+            files.push(fileList[i]);
+        }
+        return new Promise<IMarkusResponse[]>((resolve, reject) => {
+            this.uploadMultipleImageByFiles(files, key, tags).then((result: IMarkusResponse[]) => {
+                resolve(result);
+            }).catch((reason: any) => {
+                reject(reason);
+            });
+        });
     }
 }
